ebe330d3320cff9420c31f436e0f2c69d27439c6
zhan7318 <100392334+zhan7318@users.noreply.github.com>
2023-05-09 15:37:44 -0500

    Add files via upload

--- motor.m
+++ motor.m
@@ -1,4 +1,4 @@
-function [] = motor(a)
+function [] = motor(a,reverse)
 % Running motor through Arduino for touch screen task
 % Code adapted from https://lastminuteengineers.com/l298n-dc-stepper-driver-arduino-tutorial/
 % Motor A connections
@@ -9,14 +9,21 @@ function [] = motor(a)
     writeDigitalPin(a,in3,0);
     writeDigitalPin(a,in4,0);
 
-    for i = 0:1 %for 1/2 second  
-        % Set motors to 3/5 speed
+    for i = 0:3 %for 3 seconds  
+        % Set motors to max speed
         writePWMVoltage(a,enB,3);
-    
-        % Turn on motor 
-        writeDigitalPin(a,in3,1);
-        writeDigitalPin(a,in4,0);
-        pause(0.5)
+
+        if reverse == 1
+            % motor in reverse
+            writeDigitalPin(a,in3,0);
+            writeDigitalPin(a,in4,1);
+            pause(0.1)
+        else
+            % Turn on motor 
+            writeDigitalPin(a,in3,1);
+            writeDigitalPin(a,in4,0);
+            pause(0.1)
+        end
     end
     writeDigitalPin(a,in3,0);
     writeDigitalPin(a,in4,0);
